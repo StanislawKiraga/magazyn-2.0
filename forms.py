6c5df2182f4d0a2237ca3e87fbb72faf37dce850
StanislawKiraga <kiragasta@gmail.com>
2023-05-15 16:25:05 +0200

    init commit 	new file:   forms.py 	new file:   templates/base.html 	new file:   templates/dashboard.css 	new file:   templates/dashboard.js 	new file:   templates/homepage.html 	new file:   templates/product_list.html

---
+++ forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, DecimalField
+from wtforms.validators import DataRequired
+
+class ProductForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    quantity = IntegerField('Quantity', validators=[DataRequired()])
+    unit = StringField('Unit', validators=[DataRequired()])
+    unit_price = DecimalField('Unit price', validators=[DataRequired()])
